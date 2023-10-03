db05dd78bc734965a950fd0a7ff83394fb21f1f9
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2023-10-04 00:16:20 +0600

    Configuration is done in appsetting.json

--- SerilogExample/Program.cs
+++ SerilogExample/Program.cs
@@ -8,11 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-Log.Logger=new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .WriteTo.Console()
-    .WriteTo.File("logs/myBeautifulLog-.txt",rollingInterval:RollingInterval.Day)
-    .CreateLogger();
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration).CreateLogger(); 
+    //.MinimumLevel.Information()
+    //.WriteTo.Console()
+    //.WriteTo.File("logs/myBeautifulLog-.txt",rollingInterval:RollingInterval.Day)
+    //.CreateLogger();
 
 var app = builder.Build();
 
